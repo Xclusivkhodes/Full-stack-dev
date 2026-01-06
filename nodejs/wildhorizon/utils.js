18c55b1685ad012d0a449a6802c63bb0cc0bc515
XclusivKhodes <xclusivkhoder@gmail.com>
2026-01-06 11:43:16 +0000

    Made WildHorizon a public API

--- nodejs/wildhorizon/utils.js
+++ nodejs/wildhorizon/utils.js
@@ -1,6 +1,8 @@
 export const JSONData = (res, statusCode, payload) => {
   res.statusCode = statusCode;
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   res.end(JSON.stringify(payload));
 };
 
