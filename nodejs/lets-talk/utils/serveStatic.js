b23ffc54c8cc8b6b32ca777995f9ecc586d7439c
XclusivKhodes <xclusivkhoder@gmail.com>
2026-01-28 19:47:40 +0000

    Added ejs and I hope it works

--- nodejs/lets-talk/utils/serveStatic.js
+++ nodejs/lets-talk/utils/serveStatic.js
@@ -1,24 +1,41 @@
 import path from "node:path";
 import fs from "node:fs/promises";
+import ejs from "ejs"; // Import EJS
 import { sendResposne } from "./sendResponse.js";
 import { mimeConverter } from "./mimeConverter.js";
 
 export const serveStatic = async (basePath, res, req) => {
   const publicDir = path.join(basePath, "public");
-  const filePath = path.join(
-    publicDir,
-    req.url === "/" ? "index.html" : req.url,
-  );
+  const urlPath = req.url === "/" ? "index.html" : req.url;
+  const filePath = path.join(publicDir, urlPath);
   const ext = path.extname(filePath);
   const errFile = path.join(publicDir, "404.html");
+
   try {
-    const content = await fs.readFile(filePath);
-    sendResposne(res, mimeConverter(ext), content, 200);
-  } catch (err) {
-    const content = await fs.readFile(errFile);
-    if (err.code === "ENOENT") {
-      sendResposne(res, "text/html", content, 404);
+    // If the file is HTML, render it via EJS
+    if (ext === ".html") {
+      // Pass any dynamic data you want in the second argument
+      const renderedHtml = await ejs.renderFile(filePath, {
+        title: "My Dynamic Page",
+        user: "Guest",
+      });
+      sendResposne(res, "text/html", renderedHtml, 200);
+    } else {
+      // Otherwise serve as normal static file (CSS, JS, Images)
+      const content = await fs.readFile(filePath);
+      sendResposne(res, mimeConverter(ext), content, 200);
     }
-    console.log(err);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      try {
+        const errorContent = await ejs.renderFile(errFile, {
+          error: "404 Not Found",
+        });
+        sendResposne(res, "text/html", errorContent, 404);
+      } catch (ejsErr) {
+        sendResposne(res, "text/plain", "404 Not Found", 404);
+      }
+    }
+    console.error(err);
   }
 };
