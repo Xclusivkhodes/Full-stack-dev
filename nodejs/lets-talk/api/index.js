335dca307d3520ce81182e01ba4bc00cdbced74e
XclusivKhodes <xclusivkhoder@gmail.com>
2026-01-28 20:13:13 +0000

    Charley

---
+++ nodejs/lets-talk/api/index.js
@@ -0,0 +1,33 @@
+import fs from "node:fs/promises";
+
+import { serveStatic } from "../utils/serveStatic.js";
+import { handleGet, handlePost } from "../handlers/routeHandlers.js";
+import { handleNews } from "../handlers/routeHandlers.js";
+
+const __dirname = process.cwd();
+
+// Export as a default handler for Vercel
+export default async (req, res) => {
+  try {
+    if (req.url === "/api") {
+      if (req.method === "GET") {
+        handleGet(res);
+      } else if (req.method === "POST") {
+        handlePost(req, res);
+      }
+    } else if (req.url === "/api/news") {
+      return await handleNews(res);
+    } else if (!req.url.startsWith("/api")) {
+      return await serveStatic(__dirname, res, req);
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("404 Not Found");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Internal Server Error");
+  }
+};
